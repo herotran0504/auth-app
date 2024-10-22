8c338c2fc049aea900f70d4689e63e1e3b34a93e
herotran0504 <herotran0504@gmail.com>
2024-10-22 07:36:05 -0500

    Update CI build

--- deploy.sh
+++ deploy.sh
@@ -1,28 +1,38 @@
+# Constants for deployment configuration
 STACK_NAME=auth-service-fe-dev
-REGION=us-east-1
-CLI_PROFILE=user1
+AWS_REGION=us-east-1
+AWS_PROFILE=user1
 
-S3_BUCKET_NAME=hs-auth-app-frontend
-GITHUB_REPO_NAME=auth-app
+# S3 bucket and GitHub configuration
+FRONTEND_S3_BUCKET_NAME=hs-auth-app-frontend
+GITHUB_REPOSITORY_NAME=auth-app
 GITHUB_BRANCH_NAME=main
-GITHUB_OWNER=herotran0504
-GITHUB_OAUTH_TOKEN=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
+GITHUB_ACCOUNT_OWNER=herotran0504
+GITHUB_ACCESS_TOKEN=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
 
-echo -e "\n\n=========== Deploying FrontEnd =========="
+# Deploy the CloudFormation stack using the specified template and parameters
+echo -e "\n\n=========== Starting Frontend Deployment =========="
 aws cloudformation deploy \
-  --region $REGION \
-  --profile $CLI_PROFILE \
+  --region $AWS_REGION \
+  --profile $AWS_PROFILE \
   --stack-name $STACK_NAME \
   --template-file frontend/frontend.yml \
   --no-fail-on-empty-changeset \
   --capabilities CAPABILITY_NAMED_IAM \
-  --parameter-overrides S3BucketName=$S3_BUCKET_NAME GitHubRepoName=$GITHUB_REPO_NAME GitHubBranchName=$GITHUB_BRANCH_NAME GitHubOwner=$GITHUB_OWNER GitHubOAuthToken=$GITHUB_OAUTH_TOKEN
+  --parameter-overrides FrontendS3BucketName=$FRONTEND_S3_BUCKET_NAME \
+    GitHubRepoName=$GITHUB_REPOSITORY_NAME \
+    GitHubBranchName=$GITHUB_BRANCH_NAME \
+    GitHubOwner=$GITHUB_ACCOUNT_OWNER \
+    GitHubOAuthToken=$GITHUB_ACCESS_TOKEN \
+  --debug
 
+# Check if the deployment was successful
 if [ $? -eq 0 ]; then
+  echo -e "\n\n=========== Deployment Successful! Retrieving CloudFront URL =========="
+  # Retrieve and display the CloudFront URL from the CloudFormation stack outputs
   aws cloudformation list-exports \
-    --profile $CLI_PROFILE \
+    --profile $AWS_PROFILE \
     --query "Exports[?Name=='CloudFrontURL'].Value"
-  
 else
-  echo "CloudFormation deployment failed!"
+  echo "Error: CloudFormation deployment failed!"
 fi
